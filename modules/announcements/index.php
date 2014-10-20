1a5edb8d13bec3e4aac9270e0de2b68c0b309ed2
mkontogiorgis <mkontogiorgis@yahoo.gr>
2014-10-20 23:27:13 +0300

    code for coutries targeting added (but post not working)

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -343,9 +343,33 @@ if ($is_editor) {
             $message = "<p class='success'>$langAnnAdd</p>";
         }
         
+    
 		// Facebook API call
+		$myUrlLink = urlencode('http://localhost/openeclass/modules/announcements/index.php?course=' . $course_code . '&an_id=' . $id);
+		
+		preg_match_all('/(?!\b)(#\w+\b)/', $_POST['newContent'], $hashTags);
+
+		if (count($hashTags) > 0)
+		{
+			$target_country = "'countries':[";
+			foreach ($hashTags[0] as $hashtag)
+			{
+				$target_country .= "'" . ltrim($hashtag, '#') . "',";
+			}	
+			$target_country = substr($target_country, 0, -1);
+			$target_country .= "]";
+		}
+		else
+		{
+			$target_country = "";
+		}
+		//echo $target_country ;
+
+
 		$url = 'https://graph.facebook.com/v2.1/695730993849543/feed?access_token=CAANapFfgn3QBAA1reXj15nCo4RgZB3cEViKnXe0i0dTDnjhirBYYjVTv46sPL6sVosAR1L832I5wvlc3ObX4JCaZA8hubsW1qgEz0sS1bpuuDQKLZCAmMEY8guSz0BiNqQwEbpiSauM0wqwtW299p8BBzJUkTVtPMaJJNSCct3baXAwY1gy';
-		$fields = array('message' => urlencode(strip_tags($_POST['newContent'])));
+		$fields = array('link' => $myUrlLink,
+						'targeting' => $target_country,
+						'message' => urlencode(strip_tags($_POST['newContent'])));
 		//url-ify the data for the POST
 		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 		rtrim($fields_string, '&');

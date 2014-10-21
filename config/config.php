edcbbee9bab6f4ee8f217ac4c12049154a9424bb
mkontogiorgis <mkontogiorgis@yahoo.gr>
2014-10-21 20:51:51 +0300

    1. code for config/config.php & modules/work/index.php added 2. permissions and ownership changed for all /openeclass folder

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-18 16:46
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = 'r00t';
+$mysqlMainDb = 'openeclass';
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
